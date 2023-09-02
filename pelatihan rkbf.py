c35c18c3486bb8ae30c32a3139e4da176fc6f8d3
suryabaihaqi <143860106+suryabaihaqi@users.noreply.github.com>
2023-09-02 11:00:49 +0700

    Add files via upload
    upload file

---
+++ pelatihan rkbf.py
@@ -0,0 +1,21 @@
+nama_saya = "surya baihaqi"
+
+print(nama_saya, "bertipe", type(nama_saya))
+
+umur = 19
+print (umur, "bertipe",type(umur))
+
+umur = 19
+print (umur, "bertipe",type(umur))
+umur = 19
+print (umur, "bertipe",type(umur))
+
+
+
+
+ipk = 4.00
+print (ipk , "bertipe",type(ipk))
+
+menikah = False
+print('menikah')
+print(nama_saya, "bertipe", type(nama_saya))
